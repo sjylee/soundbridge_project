5d4b5c6db84e5fe5ade92edaacc0f231dbfdc2b6
Stella Lee <stella653131@gmail.com>
2026-04-04 04:21:38 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "sonner"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -56,6 +57,7 @@ function App() {
           <AuthenticatedApp />
         </Router>
         <Toaster />
+        <SonnerToaster position="top-right" theme="dark" richColors />
       </QueryClientProvider>
     </AuthProvider>
   )
